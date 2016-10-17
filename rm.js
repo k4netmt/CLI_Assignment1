3cf15b0fe93d4a4b1f46c0d359c2c7cf3004de20
Android_HungTran_12373\admin <k4netmt@gmail.com>
2016-10-17 15:38:12 +0700

    Fix command rm

--- rm.js
+++ rm.js
@@ -3,43 +3,66 @@ require('./help')
 
 var fs = require('fs')
 var path = require('path')
-var util = require('util')
-var statMode = require('stat-mode');
-var dateFormat = require('dateformat');
 
 function removeFolder(pathCurrent) {
-    var flag = fs.existsSync(pathCurrent)
-    if (flag) {
-        if (fs.existsSync(pathCurrent)) {
-            fs.stat(pathCurrent,function (err,stats) {
-                if(stats.isDirectory()){
-                    fs.readdirSync(pathCurrent).forEach(function (file, index) {
-                        var curPath = pathCurrent + "\\" + file;
-                        if (fs.lstatSync(curPath).isDirectory()) { // recurse
-                            removeFolder(curPath);
-                        } else { // delete file
-                            fs.unlinkSync(curPath);
-                        }
-                    });
-                    fs.rmdirSync(pathCurrent);
-                }else{
-                    fs.unlinkSync(pathCurrent);
-                }
-            })
+    var list = fs.readdirSync(pathCurrent);
+    for (var i = 0; i < list.length; i++) {
+        var filename = path.join(pathCurrent, list[i]);
+        var stat = fs.statSync(filename);
+        if (filename == "." || filename == "..") {
+            // pass these files
+        } else if (stat.isDirectory()) {
+            // rmdir recursively
+            removeFolder(filename);
+        } else {
+            fs.unlinkSync(filename);
         }
     }
+    fs.rmdirSync(pathCurrent);
 }
+
+function rm(pathCurrent){
+    //console.log(pathCurrent)
+    if (fs.existsSync(pathCurrent)) {
+        fs.stat(pathCurrent, function (err, stats) {
+            if (err) return
+            if (stats.isDirectory()) {
+                rmdir(pathCurrent)
+            } else {
+                fs.unlinkSync(pathCurrent);
+            }
+        })
+    }
+}
+var rmdir = function (dir) {
+
+    var list = fs.readdirSync(dir);
+    for (var i = 0; i < list.length; i++) {
+        var filename = path.join(dir, list[i]);
+        var stat = fs.statSync(filename);
+        if (filename == "." || filename == "..") {
+            // pass these files
+        } else if (stat.isDirectory()) {
+            // rmdir recursively
+            rmdir(filename);
+        } else {
+            // rm fiilename
+            fs.unlinkSync(filename);
+        }
+    }
+    fs.rmdirSync(dir);
+};
 
 function main() {
     var firstArgv = process.argv[1]
     var secondArgv = process.argv[2]
-    if (secondArgv === './' || secondArgv === 'undefine')
+    if (secondArgv === './' || secondArgv === 'undefined')
         return
     if (secondArgv.indexOf('./') === 0) {
         var subPath = secondArgv.substr(2, secondArgv.length)
-        subPath=subPath.replace('/','\\')
-        var fullPath = path.dirname(firstArgv) + '\\' + subPath
-        removeFolder(fullPath)
+        subPath = subPath.replace('/', '\\')
+        var fullPath = path.join(path.dirname(firstArgv), subPath)
+        rm(fullPath)
     }
 }
 
